b92a722fcae7b3ac8e6ef1c61750778c3ebc297d
Muhammad Farooq <fm6182218@gmail.com>
2025-11-16 23:41:30 +0500

    React Routing start

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="flex justify-between items-center px-20 shadow-xl bg-gray-200">
+      <div>
+        <img
+          src="https://media.istockphoto.com/id/1268467641/photo/small-portion-salad.jpg?s=612x612&w=0&k=20&c=9Aqf25o5eeW9qbL8Em40alPm6_KGf5-8Zat-aeEZZw4="
+          alt=""
+          className="w-15 rounded-full h-15"
+        />
+      </div>
+      <div className="flex gap-8 text-xl">
+        <Link to="/" className="text-gray-500 hover:text-blue-500">
+          {" "}
+          Home
+        </Link>
+        <Link to="/products" className="text-gray-500 hover:text-blue-500">
+          {" "}
+          products
+        </Link>
+        <Link to="/cart" className="text-gray-500 hover:text-blue-500">
+          {" "}
+          Card
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Colections.jsx
@@ -0,0 +1,145 @@
+import { getVisibleProducts } from "../data/product-filters";
+import { useState } from "react";
+import { priceRange } from "../data/products";
+
+import CategoryFilter from "../components/CategoryFilter";
+import Products from "../components/Products";
+import Ratingfilter from "../components/Ratingfilter";
+import Pricefilter from "../components/Pricefilter";
+import SortingFilter from "../components/SortingFilter";
+import Pagination from "../components/Pagination";
+import Chipfilter from "../components/chipfilter";
+
+function Colections() {
+  // Rating Filter Logic
+  const [selectedRatings, setSelectedRatings] = useState([]);
+
+  const onChangeRatingHandler = (rating, isChecked) => {
+    if (isChecked) {
+      setSelectedRatings([...selectedRatings, rating]);
+    } else {
+      setSelectedRatings(selectedRatings.filter((r) => r !== rating));
+    }
+  };
+
+  // Category Filter Logic
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const onChangeCategoryHandler = (category, isChecked) => {
+    if (isChecked) {
+      setSelectedCategories([...selectedCategories, category]);
+    } else {
+      setSelectedCategories(
+        selectedCategories.filter((cat) => cat !== category)
+      );
+    }
+  };
+
+  // Price Filter Logic
+  const initPriceFilter = {
+    min: priceRange.min,
+    max: priceRange.max,
+    isApplied: false,
+  };
+  const [initPriceRange, setInitPriceRange] = useState(initPriceFilter);
+
+  const filteredProducts = getVisibleProducts(
+    selectedCategories,
+    selectedRatings,
+    initPriceRange
+  );
+
+  // Product Sorting Filter Logic
+  const [selectedSort, setSelectedSort] = useState("");
+
+  const sortProducts = (sortValue) => {
+    setSelectedSort(sortValue);
+  };
+
+  const sortedProducts = [...filteredProducts];
+
+  if (selectedSort === "ratingHightToLow") {
+    sortedProducts.sort((a, b) => b.rating - a.rating);
+  } else if (selectedSort === "ratingLowToHight") {
+    sortedProducts.sort((a, b) => a.rating - b.rating);
+  } else if (selectedSort === "PriceHightToLow") {
+    sortedProducts.sort((a, b) => b.price - a.price);
+  } else if (selectedSort === "PriceLowToHight") {
+    sortedProducts.sort((a, b) => a.price - b.price);
+  }
+
+  // Pagination Filter Logic
+  // Pagination Logic
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 6;
+
+  const totalItems = sortedProducts.length;
+
+  const startIndex = (currentPage - 1) * pageSize;
+
+  const endIndex = startIndex + pageSize;
+  const paginatedProducts = sortedProducts.slice(startIndex, endIndex);
+  console.log(selectedCategories);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div>
+      <div className="grid grid-cols-12 gap-3 my-2 mx-2">
+        <div className="col-span-2">
+          <button className="relative p-2 w-full border-2 overflow-hidden rounded-xl bg-black text-white cursor-pointer group">
+            <span className="relative z-10">Themes</span>
+            <span
+              className="absolute top-0 left-0 w-full h-full rounded-xl 
+             hover:border-none bg-gray-500 scale-x-0 origin-left transition-transform group-hover:scale-x-100"
+            ></span>
+          </button>
+          <hr className="my-2" />
+          {/* Category Filter */}
+          <CategoryFilter
+            selectedCategories={selectedCategories}
+            onChangeCategory={onChangeCategoryHandler}
+          />
+
+          {/* Price Filter */}
+          <Pricefilter
+            init={initPriceRange}
+            price={priceRange}
+            setfun={setInitPriceRange}
+          />
+          {/* Rating Filter */}
+          <Ratingfilter
+            selectedRatings={selectedRatings}
+            onChangeRating={onChangeRatingHandler}
+          />
+          <button className="relative mb-2 p-2 w-full border-2 overflow-hidden rounded-xl bg-black text-white cursor-pointer group">
+            <span className="relative z-10">Clear All</span>
+            <span
+              className="absolute top-0 left-0 w-full h-full rounded-xl 
+             hover:border-none bg-gray-500 scale-x-0 origin-left transition-transform group-hover:scale-x-100"
+            ></span>
+          </button>
+          <SortingFilter click={sortProducts} />
+        </div>
+
+        <div className="col-span-10">
+          <Chipfilter seleted={selectedCategories} />
+
+          <hr className="my-2" />
+          <Products products={paginatedProducts} />
+
+          <Pagination
+            totalItems={totalItems}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Colections;
