32964a3d2c3e6aaf26b4928af0a9bd3d410e91f6
Muhammad Farooq <fm6182218@gmail.com>
2025-11-18 00:49:58 +0500

    UI section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="w-full h-full bg-black">
+      <div className="flex justify-center items-center">
+        <img
+          src="https://admin.broadwaypizza.com.pk/images/banner/combo%20boxes.jpg"
+          alt="connection page"
+          className="w-[90%] h-[30%] text-center mt-2 rounded-2xl"
+        />
+      </div>
+      <hr className="border-2 border-white ml-10 mr-10 mt-10" />
+
+      <div className="border-white border-2 w-[30%] h-[30%] m-10 rounded-2xl p-4">
+        <img
+          src="https://potatox.pk/wp-content/uploads/2025/06/pizza-deal-4.png"
+          alt="Product image"
+          className="rounded-2xl"
+        />
+        <h1 className="mt-2 text-3xl font-bold">Pizza-Deal-4</h1>
+        <p className="mt-2 text-gray-400">
+          Hot, cheesy, and loaded with your favorite toppings – every slice is a
+          slice of happiness!
+        </p>
+      </div>
     </div>
   );
 }
